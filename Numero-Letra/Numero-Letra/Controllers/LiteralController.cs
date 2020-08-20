18ea45843a07e69d333873790a8eaa9b709f4125
ArtMichel <a2019116758@estudiantes.upsa.edu.bo>
2020-08-19 20:26:21 -0400

    Se agrego la clase Conversores

---
+++ Numero-Letra/Numero-Letra/Controllers/LiteralController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Numero_Letra.Helpers;
+
+namespace Numero_Letra.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LiteralController : ControllerBase{
+        [HttpGet]
+        public string Literal_Get(int numero)
+        {
+            return Conversores.NumeroALetras(numero);
+        }
+        [HttpPost]
+        public string Literal_Post([FromHeader]int numero)
+        {
+            return Conversores.NumeroALetras(numero);
+        }
+    }
+}
